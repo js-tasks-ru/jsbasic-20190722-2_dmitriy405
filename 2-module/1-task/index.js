07c9149975f387e307bd9815cbaf946393300c6e
Dmitriy Mityaev <mdy8877@gmail.com>
2019-08-10 13:39:44 +0500

    глубокое клонирование

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,17 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+ 	
 function clone(obj) {
+var copy;
+if (null == obj || "object" != typeof obj) return obj;
+// Обработка объектов
+  if (obj instanceof Object) {
+      copy = {};
+      for (var attr in obj) {
+          if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+      }
+      return copy;
+  }
+
 }
