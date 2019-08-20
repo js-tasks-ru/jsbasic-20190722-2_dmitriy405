686440b6a815f39b4500234891b866f0afa50c56
Dmitriy Mityaev <mdy8877@gmail.com>
2019-08-20 22:53:09 +0500

    min max

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,13 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+	let arr = str.split(' ').join(',').split(',');
+		for (let i = 0; i < arr.length; i++) {
+		  arr[i] = parseFloat(arr[i]);
+		}
+	let newArr = arr.filter(Boolean);
+	let min = Math.min.apply(null, newArr);
+	let max = Math.max.apply(null, newArr);
+	let arrMinMax = [];
+	return arrMinMax.push(min, max);	
 }
